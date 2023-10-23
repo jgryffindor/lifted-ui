16b8986d47eaa1506b07ae5ac92a5e00d95779b3
Justin Griffin <jgriffin555@comcast.net>
2023-10-23 17:55:44 -0500

    fix: Test gray 100

--- src/theme/foundation/styles.ts
+++ src/theme/foundation/styles.ts
@@ -3,8 +3,8 @@ import { mode } from "@chakra-ui/theme-tools";
 export const styles = {
   global: (props: Record<string, any>) => ({ 
     "html,body,#root": {
-      bg: mode("lifted.gray.600","lifted.gray.50")(props),
-      bgColor: mode("lifted.gray.600","lifted.gray.50")(props),
+      bg: mode("lifted.gray.600","lifted.gray.100")(props),
+      bgColor: mode("lifted.gray.600","lifted.gray.100")(props),
       color: mode("white","black")(props),
       height: "100%",
     },
