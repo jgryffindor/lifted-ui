cc7b0bb9b2163cbc32b3485503ea5689be4ae962
Justin Griffin <jgriffin555@comcast.net>
2023-10-25 09:41:46 -0500

    feat: Setting solid variant on code component

--- src/theme/components/code.ts
+++ src/theme/components/code.ts
@@ -1,7 +1,9 @@
 import { mode } from "@chakra-ui/theme-tools";
 
 export const Code = {
-  baseStyle: (props: Record<string, any>) => ({
-    bg: mode("lifted.gray.700","lifted.gray.100")(props),
-  }),
+  variants: {
+    solid: (props: Record<string, any>) => ({
+      bg: mode("lifted.gray.700","lifted.gray.100")(props),
+    }),
+  }
 };
