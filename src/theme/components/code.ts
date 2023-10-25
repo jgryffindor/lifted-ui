89d15d5a94876d2d151c6c1a6fa1f09c7f37ef59
Justin Griffin <jgriffin555@comcast.net>
2023-10-25 13:30:04 -0500

    feat: Set colorscheme on code component

--- src/theme/components/code.ts
+++ src/theme/components/code.ts
@@ -1,7 +1,7 @@
 import { mode } from "@chakra-ui/theme-tools";
 
 export const Code = {
-  baseStyle: (props: Record<string, any>) => ({
-    bg: mode("lifted.gray.700","lifted.gray.100")(props),
+  defaultProps: (props: Record<string, any>) => ({
+    colorScheme: mode("lifted.gray.700","lifted.gray.100")(props),
   }),
 };
