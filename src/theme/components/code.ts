5906984cc5918d3065ac61842153e7ae520d6499
Justin Griffin <jgriffin555@comcast.net>
2023-10-25 10:20:31 -0500

    feat: Export code component and enable in theme

--- src/theme/components/code.ts
+++ src/theme/components/code.ts
@@ -1,6 +1,9 @@
 import { mode } from "@chakra-ui/theme-tools";
 
 export const Code = {
+  defaultProps: {
+    variant: "solid",
+  },
   variants: {
     solid: (props: Record<string, any>) => ({
       bg: mode("lifted.gray.700","lifted.gray.100")(props),
