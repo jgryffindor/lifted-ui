3a4a3fa0a7b69f06e53c7526169348c9290cc507
Justin Griffin <jgriffin555@comcast.net>
2023-10-25 13:42:48 -0500

    feat: Debug bg props on code component without mode

--- src/theme/components/code.ts
+++ src/theme/components/code.ts
@@ -1,7 +1,13 @@
 import { mode } from "@chakra-ui/theme-tools";
 
+// export const Code = {
+//   defaultProps: (props: Record<string, any>) => ({
+//     colorScheme: mode("lifted.gray.700","lifted.gray.100")(props),
+//   }),
+// };
+
 export const Code = {
-  defaultProps: (props: Record<string, any>) => ({
-    colorScheme: mode("lifted.gray.700","lifted.gray.100")(props),
-  }),
+  defaultProps: {
+    bg: "lifted.gray.700",
+  }
 };
