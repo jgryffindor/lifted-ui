c2d2d9c12af94795d5ca2418f6cee7a880e3acde
Justin Griffin <jgriffin555@comcast.net>
2023-10-25 12:10:23 -0500

    feat: Set baseStyle on Code

--- src/theme/components/code.ts
+++ src/theme/components/code.ts
@@ -1,7 +1,7 @@
 import { mode } from "@chakra-ui/theme-tools";
 
 export const Code = {
-  defaultProps: (props: Record<string, any>) => ({
+  baseStyle: (props: Record<string, any>) => ({
     bg: mode("lifted.gray.700","lifted.gray.100")(props),
   }),
 };
