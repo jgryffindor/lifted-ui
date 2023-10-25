c29cd7097bca7332195478f491bd6d966cd1ced4
Justin Griffin <jgriffin555@comcast.net>
2023-10-25 11:17:44 -0500

    feat: Test defaultProps again on code component

--- src/theme/components/code.ts
+++ src/theme/components/code.ts
@@ -1,12 +1,7 @@
 import { mode } from "@chakra-ui/theme-tools";
 
 export const Code = {
-  defaultProps: {
-    variant: "solid",
-  },
-  variants: {
-    solid: (props: Record<string, any>) => ({
-      bg: mode("lifted.gray.700","lifted.gray.100")(props),
-    }),
-  }
+  defaultProps: (props: Record<string, any>) => ({
+    bg: mode("lifted.gray.700","lifted.gray.100")(props),
+  }),
 };
