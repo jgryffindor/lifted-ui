f7f255a33215bdacfc3fe2d77ff91555f1d70fbc
Justin Griffin <jgriffin555@comcast.net>
2023-10-26 10:01:08 -0500

    fix: Flip bg values on bg colormode

--- src/theme/foundation/styles.ts
+++ src/theme/foundation/styles.ts
@@ -3,8 +3,8 @@ import { mode } from "@chakra-ui/theme-tools";
 export const styles = {
   global: (props: Record<string, any>) => ({ 
     "html,body,#root": {
-      bg: mode("lifted.gray.800","lifted.gray.100")(props),
-      bgColor: mode("lifted.gray.800","lifted.gray.100")(props),
+      bg: mode("lifted.gray.100","lifted.gray.800",)(props),
+      bgColor: mode("lifted.gray.100","lifted.gray.800",)(props),
       color: mode("white","black")(props),
       height: "100%",
     },
